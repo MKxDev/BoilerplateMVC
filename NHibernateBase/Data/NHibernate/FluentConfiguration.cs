9a8da43c04d71c682db368ab5870c5944f53bd7e
Marat Khoud <janmarat@gmail.com>
2012-11-21 23:12:03 -0800

    Initial commit

---
+++ NHibernateBase/Data/NHibernate/FluentConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace NHibernateBase.Data.NHibernate
+{
+    public class FluentConfiguration
+    {
+        public static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                    .Database(PostgreSQLConfiguration // TODO: Change flavor here!
+                        .Standard
+                        .ConnectionString(c => c.FromAppSetting("YOUR_CONNECTION_STRING_ELEMENT"))
+                    )
+                    .Mappings(m => {
+                        // m.FluentMappings.AddFromAssemblyOf<>() // TODO: Add your mappings
+                    })
+                    .BuildSessionFactory();
+        }
+    }
+}
---
+++ NHibernateBase/IoC/NHibernateRegistry.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate;
+using NHibernateBase.Data.NHibernate;
+using StructureMap.Configuration.DSL;
+
+namespace NHibernateBase.IoC
+{
+    public class NHibernateRegistry : Registry
+    {
+        public NHibernateRegistry()
+        {
+            var sessionFactory = FluentConfiguration.CreateSessionFactory();
+
+            For<ISessionFactory>()
+                .Singleton()
+                .Use(sessionFactory);
+
+            For<ISession>()
+                .HybridHttpOrThreadLocalScoped()
+                .Use(x => x.GetInstance<ISessionFactory>().OpenSession());
+        }
+    }
+}
