ddba8479c8a6a9a8624d48b7352070d50db03866
Marat K <janmarat@gmail.com>
2012-12-10 20:32:40 -0800

    Missed some files

---
+++ RepositoryBase/Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryBase.Models
+{
+    public abstract class BaseModel
+    {
+        public virtual Guid Id { get; private set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ ServiceBase/IoC/ServiceRegistry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using StructureMap.Configuration.DSL;
+
+namespace ServiceBase.IoC
+{
+    public class ServiceRegistry : Registry
+    {
+        public ServiceRegistry()
+        {
+
+        }
+    }
+}
